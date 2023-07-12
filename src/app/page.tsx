c53095b19e33c2fb281efc4a609acd5a52a4ad0f
R.P. Muhammad Bahtiar Subrata <subratabahtiar@outlook.com>
2023-07-12 20:56:51 +0700

    fix: update testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-            king gnu
+            king gnu testing update
           </p>
         </a>
       </div>
